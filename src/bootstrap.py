94e39651e2f7ce8a9ed32f263be278930783a006
gef3dx <gef3dx@gmail.com>
2025-06-05 15:49:45 +0300

    start project

---
+++ src/bootstrap.py
@@ -0,0 +1,37 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from src.core.loggers import set_logging
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Предварительная инициализация приложения.
+
+    - устанавливаем настройки логгирования
+    - устанавливаем настройки кеширования
+    - устанавливаем настройки стриминга
+    """
+    set_logging()
+
+    # await CacheManager.init(settings)
+
+    # stream_repository = await get_streaming_repository_type()
+    # await stream_repository.start(settings.kafka)
+
+    yield
+
+    # await stream_repository.stop()
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        lifespan=lifespan,
+        docs_url='/docs',
+        openapi_url='/docs.json',
+    )
+
+   # app = apply_routes(apply_middleware(app))
+
+    return app
